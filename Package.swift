b39bb31113c83872d3a9000db28d7d01f9956803
Yonat Sharon <yonat@ootips.org>
2019-08-21 23:27:32 +0300

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "RadioGroup",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library( name: "RadioGroup", targets: ["RadioGroup"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/yonat/SweeterSwift", from: "1.0.2")
+    ],
+    targets: [
+        .target(name: "RadioGroup", dependencies: ["SweeterSwift"], path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
+)
